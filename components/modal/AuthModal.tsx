d6ccfc3a8bdac40660d23b9270a5fa55cb4e809b
david emioma <davidemiomauche@gmail.com>
2023-06-08 19:05:47 +0100

    deployment issues

--- components/modal/AuthModal.tsx
+++ components/modal/AuthModal.tsx
@@ -37,7 +37,7 @@ const AuthModal = () => {
     >
       <Auth
         theme="dark"
-        providers={["github", "google"]}
+        providers={["github"]}
         magicLink
         supabaseClient={supabaseClient}
         appearance={{
